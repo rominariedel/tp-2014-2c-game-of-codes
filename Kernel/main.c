5172a058c5bde06bc376209c01089a7760f545ef
rominariedel <riedelromina@gmail.com>
2014-09-09 15:30:44 -0300

    Agrego kernel

---
+++ Kernel/main.c
@@ -0,0 +1,24 @@
+/*
+ * main.c
+ *
+ *  Created on: 09/09/2014
+ *      Author: utnso
+ */
+
+
+
+typedef struct {
+	int PID;
+	int TID;
+	int KM;
+	int baseSegmentoCodigo;
+	int tamanioSegmentoCodigo;
+	int punteroInstruccion;
+	int baseStack;
+	int cursorStack;
+	int registrosProgramacion;
+}TCB_struct;
+
+int main(){
+	return 0;
+}
