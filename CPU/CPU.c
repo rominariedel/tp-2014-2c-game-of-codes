b94bb7e3cd1f663cb230a2119dd20474f2497937
wuacamolegs <camigarciasantillan@hotmail.com>
2014-10-02 20:23:57 -0300

    instrucciones privilegiadas

--- CPU/CPU.c
+++ CPU/CPU.c
@@ -21,6 +21,7 @@
 #include <signal.h>
 #include <semaphore.h>
 #include <math.h>
+#include <error.h>
 
 
 
@@ -91,21 +92,24 @@ void limpiarRegistros();
 
 /*Funciones MSP*/
 int MSP_SolicitarProximaInstruccionAEJecutar(int* punteroAInstruccion);
+void* MSP_CrearNuevoSegmento(int PID, int tamanioSegmento);
+void MSP_DestruirSegmento(int PID, void* segmento);
+
 
 /*Instrucciones*/
 char* LOAD(char* registro, int numero);
-char* GETM(char* registroA, char* registroB);
-char* MOVR(char* registroA, char* registroB);
-char* ADDR(char* registroA, char* registroB);
-char* SUBR(char* registroA, char* registroB);
-char* MULR(char* registroA, char* registroB);
-char* MODR(char* registroA, char* registroB);
-char* DIVR(char* registroA, char* registroB);
+char* GETM(char* registro1, char* registro2);
+char* MOVR(char* registro1, char* registro2);
+char* ADDR(char* registro1, char* registro2);
+char* SUBR(char* registro1, char* registro2);
+char* MULR(char* registro1, char* registro2);
+char* MODR(char* registro1, char* registro2);
+char* DIVR(char* registro1, char* registro2);
 char* INCR(char* registro);
 char* DECR(char* registro);
-char* COMP(char* registroA, char* registroB);
-char* CGEQ(char* registroA, char* registroB);
-char* CLEQ(char* registroA, char* registroB);
+char* COMP(char* registro1, char* registro2);
+char* CGEQ(char* registro1, char* registro2);
+char* CLEQ(char* registro1, char* registro2);
 char* GOTO(char* registro);
 void JMPZ(int nro, char* registro);
 void INTE(int* direccion);
@@ -244,6 +248,7 @@ void conectarConKernel()
 
 }
 
+
 int abortarEjecucion(){
 	//avisar al kernel que hay que matar al CPU
 	return 0;
@@ -315,6 +320,7 @@ void limpiarRegistros(){
 
 /*Funciones MSP*/
 
+
 int MSP_SolicitarProximaInstruccionAEJecutar(int* punteroInstruccion){
 	int proximaInstruccionAEjecutar;
 	int mensaje[2];
@@ -324,6 +330,30 @@ int MSP_SolicitarProximaInstruccionAEJecutar(int* punteroInstruccion){
 	int status = recv(socketMSP, &proximaInstruccionAEjecutar, sizeof(char), 0);
 	//me manda la instruccion a ejecutar en forma de numero, el valor de la instruccion en ASCII
 	return proximaInstruccionAEjecutar;
+}
+
+
+
+void* MSP_CrearNuevoSegmento(int PID, int tamanioSegmento){
+	void* nuevoSegmento;
+	int mensaje[3];
+	mensaje[0]=2; //codigo de operacion 2 crear segmento
+	mensaje[1]= PID;
+	mensaje[2]= tamanioSegmento;
+	send(socketMSP,mensaje, sizeof(int[3]), 0);
+	int status = recv(socketMSP, &nuevoSegmento, sizeof(char), 0);
+	return nuevoSegmento;
+}
+
+void MSP_DestruirSegmento(int PID, void* segmento){
+	int confirmacion;
+	int mensaje[3];
+	mensaje[0]=2; //codigo de operacion 3 destruir segmento
+	mensaje[1]= PID;
+	mensaje[2]= segmento;
+	send(socketMSP,mensaje, sizeof(int[3]), 0);
+	recv(socketMSP, confirmacion, sizeof(char), 0);
+	if(confirmacion == -1){error("no se pudo destruir el segmento");}
 }
 
 
@@ -342,52 +372,62 @@ char* LOAD(char* registro, int numero){ //Carga en el registro, el número dado.
 	return registro;
 }
 
-char* GETM(char* registroA, char* registroB){ //Obtiene el valor de memoria apuntado por el segundo registro. El valor obtenido lo asigna en el primer registro.
-	*registroA = *registroB;
-	return registroA;
+char* SETM(int numero, char* registro1, char* registro2){
+	//Pone tantos bytes desde el segundo registro, hacia la memoria apuntada por el primer registro
+	//explicacion Gaston: pone en los n bytes del registro bx en la dirección de memoria apuntanda por el registro ax
+	//(ax = numero que es una posición de memoria)
+
+	//TODO: sigo sin entender.
+
+	return registro2;
 }
 
-//TODO: SETM no entiendo que tiene que hacer
 
-char* MOVR(char* registroA, char* registroB){ //Copia el valor del segundo registro hacia el primero
-	//no es lo mismo que getm?
-	return registroA;
+char* GETM(char* registro1, char* registro2){ //Obtiene el valor de memoria apuntado por el segundo registro. El valor obtenido lo asigna en el primer registro.
+	registro1 = &registro2;
+	return registro1;
 }
 
-char* ADDR(char* registroA, char* registroB){ //Suma el primer registro con el segundo registro. El resultado de la operación se almacena en el registro A.
-	char* aux = registroA;
-	*registroA = *aux + *registroB;
-	return registroA;
+/*
+ * la diferencia entre GETM y MOVR seria que en GETM , le estoy asignando al primer registro la direccion de memoria que tiene el segundo registro?
+ */
+
+char* MOVR(char* registro1, char* registro2){ //Copia el valor del segundo registro hacia el primero
+	*registro1 = *registro2;
+	return registro1;
 }
 
-char* SUBR(char* registroA, char* registroB){ //Resta el primer registro con el segundo registro. El resultado de la operación se almacena en el registro A.
-	char* aux = registroA;
-	*registroA = *aux - *registroB;
-	return registroA;
+char* ADDR(char* registro1, char* registro2){ //Suma el primer registro con el segundo registro. El resultado de la operación se almacena en el registro A.
+	*A = *registro1 + *registro2;
+	return A;
 }
 
-char* MULR(char* registroA, char* registroB){ //Multiplica el primer registro con el segundo registro. El resultado de la operación se almacena en el registro A.
-	char* aux = registroA;
-	*registroA = (*aux) * (*registroB);
-	return registroA;
+char* SUBR(char* registro1, char* registro2){ //Resta el primer registro con el segundo registro. El resultado de la operación se almacena en el registro A.
+	*A = *registro1 - *registro2;
+	return A;
 }
 
-char* MODR(char* registroA, char* registroB){ //Obtiene el resto de la división del primer registro con el segundo registro. El resultado de la operación se almacena en el registro A.
-	char* aux = registroA;
-	*registroA = (*aux) % (*registroB);
-	return registroA;
+char* MULR(char* registro1, char* registro2){ //Multiplica el primer registro con el segundo registro. El resultado de la operación se almacena en el registro A.
+	*A = (*registro1) * (*registro2);
+	return A;
 }
 
-char* DIVR(char* registroA, char* registroB){
+char* MODR(char* registro1, char* registro2){ //Obtiene el resto de la división del primer registro con el segundo registro. El resultado de la operación se almacena en el registro A.
+	*A = (*registro1) % (*registro2);
+	return A;
+}
+
+char* DIVR(char* registro1, char* registro2){
 	//Divide el primer registro con el segundo registro. El resultado de la operación se almacena en el registro A; a menos que el segundo operando sea 0,
-	//en cuyo caso se asigna el flag de ZERO_DIV y no se hace la operación.
-	if(*registroB == 0){
-		*registroB = 0;
-	} else {
-		char* aux = registroA;
-		*registroA = (*aux) % (*registroB);
-		}
-	return registroA;
+	//en cuyo caso tira error de division por cero
+
+	if(*registro2 == 0){
+		error("division por cero");
+	}
+	else {
+		*A = (*registro1) % (*registro2);
+	}
+	return A;
 }
 
 char* INCR(char* registro){ //incrementar una unidad al registro
@@ -400,52 +440,64 @@ char* DECR(char* registro){ //decrementar una unidad al registro
 	return registro;
 }
 
-char* COMP(char* registroA, char* registroB){
+char* COMP(char* registro1, char* registro2){
 	//Compara si el primer registro es igual al segundo. De ser verdadero, se almacena el valor 1. De lo
 	//contrario el valor 0. El resultado de la operación se almacena en el registro A.
-	if(*registroA == *registroB){
-		*registroA = 1;
+	if(*registro1 == *registro2){
+		*A = 1;
 	}
-	*registroA = 0;
-	return registroA;
+	else{
+		*A = 0;
+	}
+	return A;
 }
 
-char* CGEQ(char* registroA, char* registroB){
+char* CGEQ(char* registro1, char* registro2){
 	//Compara si el primer registro es mayor o igual al segundo. De ser verdadero, se almacena el valor 1. De lo contrario el valor 0. El resultado de la operación se almacena en el registro A.
-	if(*registroA >= *registroB){
-		*registroA = 1;
+	if(*registro1 >= *registro2){
+		*A = 1;
 	}
-	*registroA = 0;
-	return registroA;
+	else{
+		*A = 0;
+	}
+	return A;
 }
 
-char* CLEQ(char* registroA, char* registroB){
+char* CLEQ(char* registro1, char* registro2){
 	//Compara si el primer registro es menor o igual al segundo. De ser verdadero, se almacena el valor 1. De lo contrario el valor 0. El resultado de la operación se almacena en el registro A.
-	if(*registroA <= *registroB){
-		*registroA = 1;
+	if(*registro1 <= *registro2){
+		*A = 1;
 	}
-	registroA = 0;
-	return registroA;
+	else{
+	    *A = 0;
+	}
+	return A;
 }
 
 char* GOTO(char* registro){
 	//Altera el flujo de ejecución para ejecutar la instrucción apuntada por el registro. El valor es el desplazamiento desde el inicio del programa.
-	//TODO: REVISAR EL TEMA DE LOS REGISTROS DE CPU.
-	*punteroInstruccionActual = *registro;
+	punteroInstruccionActual = &baseSegmentoCodigoActual + *registro;
 	return registro;
 }
 
-void JMPZ(int nro, char* registro){
-	//Altera el flujo de ejecución, solo si el valor del registro A es cero, para ejecutar la instrucción
-	//apuntada por el registro. El valor es el desplazamiento desde el inicio del programa.
-	//TODO: no tendria que tener por parametro un registro???? YO SE LO AGREGO
-	if(registro == 0){
-		GOTO(registro);
-	}
 
+void JMPZ(int direccion){
+	//Altera el flujo de ejecución sólo si el valor del registro A es cero, para ejecutar la instrucción apuntada por la Dirección.
+	//El valor es el desplazamiento desde el inicio del programa.
+
+	if(A == 0){
+		GOTO(direccion);
+	}
 }
 
-//TODO: JMPZ y JPNZ son lo mismo
+void JPNZ(int direccion){
+	// Altera el flujo de ejecución sólo si el valor del registro A no es cero, para ejecutar la instrucción apuntada por la Dirección.
+	// El valor es el desplazamiento desde el inicio del programa.
+
+	if(A != 0){
+		GOTO(direccion);
+	}
+}
 
 void INTE(int* direccion){
 
@@ -460,31 +512,23 @@ void INTE(int* direccion){
 	“MALC”, “FREE”, “INNN”, “INNC”, “OUTN”, “OUTC”, “BLOK”, “WAKE”, “CREA” y “JOIN”. Invoca al
 	servicio correspondiente en el proceso Kernel. Notar que el hilo en cuestión debe bloquearse
 	tras una interrupción.*/
-
-
 }
 
 void FLCL(){ //limpia registro de flags
 	//TODO: ver tema flags del CPU
 }
 
-void SHIF(int numero, char* registro){
+char* SHIF(int numero, char* registro){
 	//Desplaza los bits del registro, tantas veces como se indique en el Número. De ser
 	//desplazamiento positivo, se considera hacia la derecha. De lo contrario hacia la izquierda.
-/*
-	double auxNum = pow(2.0,(double)numero);
-
 	if(numero < 0){
-		char* aux = registro;
-		*registro = *aux * auxNum;
-
+		*registro = *registro << numero;
 	}
 	if(numero > 0){
-		char* aux = registro;
-		*registro = *aux / auxNum;
+		*registro = *registro >> numero;
 	}
-*/
-	}
+	return registro;
+}
 
 void NOPP(){
 	//NO HAGO NADA
@@ -496,10 +540,90 @@ void PUSH(int numeroA, int numeroB){
 }
 
 void TAKE(int numero, char* registro){
+	//Desapila los primeros bytes, indicado por el número, del stack hacia el registro. Modifica el valor del registro de stack de forma acorde.
 	//TODO:  no entiendo que seria apilar.
 }
 
 void XXXX(){
-
+	// finaliza ejecucion del programa
 	//TODO: hacer funcion. finalizarEjecucion();
 }
+
+/*Instrucciones Protegidas*/
+/*    Solo si KM == 1     */
+
+
+void MALC(){
+	//Reserva una cantidad de memoria especificada por el registro A. La direccion de esta se
+	//almacena en el registro A. Crea en la MSP un nuevo segmento del tamaño especificado asociado
+	//al programa en ejecución.
+
+	void* cantidadMemoria = *A;
+	A = &cantidadMemoria;
+	MSP_CrearNuevoSegmento(PIDactual, cantidadMemoria);
+
+}
+
+void FREE(){
+	//Libera la memoria apuntada por el registro A. Solo se podrá liberar memoria alocada por la
+	//instrucción de MALC. Destruye en la MSP el segmento indicado en el registro A.
+	//TODO:
+	MSP_DestruirSegmento(PIDactual, *A);
+}
+
+void INNN(){
+	// Pide por consola del programa que se ingrese un número, con signo entre –2.147.483.648 y
+	// 2.147.483.647. El mismo será almacenado en el registro A. Invoca al servicio correspondiente en
+	// el proceso Kernel.
+	int numero;
+	printf("Ingrese un numero entre –2.147.483.648 y 2.147.483.647");
+	scanf("%l", numero);
+	*A = numero;
+}
+
+//TODO: a que refiere con invoca al servicio correspondiente en el proceso Kernel?
+
+void INNC(){
+	//Pide por consola del programa que se ingrese una cadena no más larga de lo indicado por el
+	//registro B. La misma será almacenada en la posición de memoria apuntada por el registro A.
+	//Invoca al servicio correspondiente en el proceso Kernel.
+	char cadena[];
+	printf("Ingrese una cadena no mas larga que %d", *B);
+	scanf("%l", cadena);
+	*A = cadena;
+}
+
+
+
+/*29. OUTN
+Imprime por consola del programa el número, con signo almacenado en el registro A. Invoca al
+servicio correspondiente en el proceso Kernel.
+30. OUTC
+Imprime por consola del programa una cadena de tamaño indicado por el registro B que se
+encuentra en la direccion apuntada por el registro A. Invoca al servicio correspondiente en el
+proceso Kernel.
+31. CREA
+Crea un hilo, hijo del TCB que ejecutó la llamada al sistema correspondiente. El nuevo hilo
+tendrá su Program Counter apuntado al número almacenado en el registro B. El identificador del
+nuevo hilo se almacena en el registro A.
+Para lograrlo debe generar un nuevo TCB como copia del TCB actual, asignarle un nuevo TID
+correlativo al actual, cargar en el Puntero de Instrucción la rutina donde comenzará a ejecutar el
+nuevo hilo (registro B), pasarlo de modo Kernel a modo Usuario, duplicar el segmento de stack
+desde la base del stack, hasta el cursor del stack. Asignar la base y cursor de forma acorde (tal
+que la diferencia entre cursor y base se mantenga igual)13 y luego invocar al servicio
+correspondiente en el proceso Kernel con el TCB recién generado.
+32. JOIN
+Bloquea el programa que ejecutó la llamada al sistema hasta que el hilo con el identificador
+almacenado en el registro A haya finalizado. Invoca al servicio correspondiente en el proceso
+Kernel.
+33. BLOK
+ */
+
+
+
+
+
+
+
+
+
