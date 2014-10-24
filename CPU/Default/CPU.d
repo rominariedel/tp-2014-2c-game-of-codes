8fe46301dd839915129138fdf1e16cc0240e8b09
wuacamolegs <camigarciasantillan@hotmail.com>
2014-10-24 18:11:57 -0300

    creando libreria cpu.h

--- CPU/Default/CPU.d
+++ CPU/Default/CPU.d
@@ -1,10 +1,12 @@
-CPU.d: ../CPU.c \
+CPU.d: ../CPU.c ../CPU.h \
  /home/utnso/tp-2014-2c-game-of-codes/commons/commons/string.h \
  /home/utnso/tp-2014-2c-game-of-codes/commons/commons/log.h \
  /home/utnso/tp-2014-2c-game-of-codes/commons/commons/config.h \
  /home/utnso/tp-2014-2c-game-of-codes/commons/commons/collections/dictionary.h \
  /home/utnso/tp-2014-2c-game-of-codes/commons/commons/collections/node.h \
  /home/utnso/tp-2014-2c-game-of-codes/Sockets/sockets.h
+
+../CPU.h:
 
 /home/utnso/tp-2014-2c-game-of-codes/commons/commons/string.h:
 
