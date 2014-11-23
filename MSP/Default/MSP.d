b0761ca90cb8d63cef9607ad5ad2d18e752c7eec
toiacabrera <toiacabrera@hotmail.com>
2014-11-23 02:53:23 -0300

    arreglo swap y merge

--- MSP/Default/MSP.d
+++ MSP/Default/MSP.d
@@ -1,31 +1,6 @@
-MSP.d: ../MSP.c \
- /home/utnso/workspace/tp-2014-2c-game-of-codes/commons/commons/string.h \
- /home/utnso/workspace/tp-2014-2c-game-of-codes/commons/commons/config.h \
- /home/utnso/workspace/tp-2014-2c-game-of-codes/commons/commons/collections/dictionary.h \
- /home/utnso/workspace/tp-2014-2c-game-of-codes/commons/commons/collections/node.h \
- /home/utnso/workspace/tp-2014-2c-game-of-codes/commons/commons/collections/list.h \
- ../MSP.h \
- /home/utnso/workspace/tp-2014-2c-game-of-codes/Sockets/sockets.h \
- /home/utnso/workspace/tp-2014-2c-game-of-codes/commons/commons/error.h \
- /home/utnso/workspace/tp-2014-2c-game-of-codes/commons/commons/log.h \
- /home/utnso/workspace/tp-2014-2c-game-of-codes/commons/commons/txt.h
-
-/home/utnso/workspace/tp-2014-2c-game-of-codes/commons/commons/string.h:
-
-/home/utnso/workspace/tp-2014-2c-game-of-codes/commons/commons/config.h:
-
-/home/utnso/workspace/tp-2014-2c-game-of-codes/commons/commons/collections/dictionary.h:
-
-/home/utnso/workspace/tp-2014-2c-game-of-codes/commons/commons/collections/node.h:
-
-/home/utnso/workspace/tp-2014-2c-game-of-codes/commons/commons/collections/list.h:
+MSP.d: ../MSP.c ../MSP.h \
+ /home/utnso/workspace/tp-2014-2c-game-of-codes/Sockets/sockets.h
 
 ../MSP.h:
 
 /home/utnso/workspace/tp-2014-2c-game-of-codes/Sockets/sockets.h:
-
-/home/utnso/workspace/tp-2014-2c-game-of-codes/commons/commons/error.h:
-
-/home/utnso/workspace/tp-2014-2c-game-of-codes/commons/commons/log.h:
-
-/home/utnso/workspace/tp-2014-2c-game-of-codes/commons/commons/txt.h:
