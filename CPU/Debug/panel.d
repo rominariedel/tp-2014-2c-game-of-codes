19f6de2505629ba14536ff0673359733d417fe01
wuacamolegs <camigarciasantillan@hotmail.com>
2014-12-02 10:01:38 -0300

    borro carpetas innecesarias

--- CPU/Debug/panel.d
+++
@@ -1,7 +0,0 @@
-panel.d: ../panel.c ../panel.h ../kernel.h ../cpu.h
-
-../panel.h:
-
-../kernel.h:
-
-../cpu.h:
--- CPU/Debug/panel/commons/error.d
+++
@@ -1,6 +0,0 @@
-panel/commons/error.d: ../panel/commons/error.c ../panel/commons/error.h \
- ../panel/commons/string.h
-
-../panel/commons/error.h:
-
-../panel/commons/string.h:
